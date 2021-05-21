cd3399cb960f99ca5f3e63fc99eae79718f1da42
piranjamk <53086914+piranjamk@users.noreply.github.com>
2021-05-21 10:46:18 +0200

    robot

---
+++ INZYNIERKA/setups.h
@@ -0,0 +1,13 @@
+#pragma once
+/*
+ * deklaracje dla konfiguracji poszczegolnych modulow
+ */
+
+void set_piny_kontrolne_mostka();
+void set_piny_pwm_mostka();
+void set_przycisk();
+void set_pwm();
+void set_usart_printowanie();
+void set_i2c();
+void init_servo();
+void set_ultrasound_pins();
